386e9f4144b424594f7d231171ed332c54fb5c5c
lauhoyin3a <lauhoyin464866@gmail.com>
2023-05-18 23:35:07 +0800

    kafka intial setup

--- src/main/java/com/spring/ebank/EbankApplication.java
+++ src/main/java/com/spring/ebank/EbankApplication.java
@@ -1,7 +1,11 @@
 package com.spring.ebank;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +16,15 @@ public class EbankApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EbankApplication.class, args);
 	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate){
+		return args -> {
+		kafkaTemplate.send("ebank", "hello kafka");
+		};
+	}
 @GetMapping("/123")
 	public String apiRoot(){return "Hello!";}
+
+
 }

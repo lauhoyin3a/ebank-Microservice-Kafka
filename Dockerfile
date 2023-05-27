4a48055aebad95494b90426ef7965de69e84b9ae
lauhoyin3a <lauhoyin464866@gmail.com>
2023-05-27 12:06:40 +0800

    Handling schema evolution, logging and monitoring

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM openjdk:17
 WORKDIR /app
 
-COPY target/ebank-0.0.1-SNAPSHOT.jar build/app.jar
-ENTRYPOINT ["java", "-jar", "build/app.jar"]
+COPY target/ebank-0.0.1-SNAPSHOT.jar app.jar
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/com/spring/ebank/transaction/TransactionController.java
+++ src/main/java/com/spring/ebank/transaction/TransactionController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("ap")
+@RequestMapping("api")
 public class TransactionController {
     private JsonKafkaProducer kafkaProducer;
 
